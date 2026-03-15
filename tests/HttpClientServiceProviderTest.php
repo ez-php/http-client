648bdc12b2f5fde684764754a0e1343657849e10
home <andreas.uretschnig@gmail.com>
2026-03-16 00:10:13 +0100

    Refactor `HttpClientServiceProviderTest` to use a minimal `ContainerInterface` implementation, replacing `Application` dependencies, and update ez-php packages to latest versions.

--- tests/HttpClientServiceProviderTest.php
+++ tests/HttpClientServiceProviderTest.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\HttpClient;
 
-use EzPhp\Application\Application;
-use EzPhp\Exceptions\ApplicationException;
-use EzPhp\Exceptions\ContainerException;
+use EzPhp\Contracts\ContainerInterface;
 use EzPhp\HttpClient\CurlTransport;
 use EzPhp\HttpClient\Http;
 use EzPhp\HttpClient\HttpClient;
@@ -16,7 +14,6 @@ use EzPhp\HttpClient\HttpResponse;
 use EzPhp\HttpClient\TransportInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\UsesClass;
-use ReflectionException;
 use Tests\TestCase;
 
 /**
@@ -42,63 +39,103 @@ final class HttpClientServiceProviderTest extends TestCase
     }
 
     /**
-     * @throws ReflectionException
-     * @throws ApplicationException
-     * @throws ContainerException
+     * Build a minimal container stub and boot the provider against it.
+     */
+    private function makeBootedContainer(): ContainerInterface
+    {
+        $container = new class () implements ContainerInterface {
+            /** @var array<string, callable> */
+            private array $bindings = [];
+
+            /** @var array<string, object> */
+            private array $instances = [];
+
+            public function bind(string $abstract, string|callable|null $factory = null): void
+            {
+                if (is_callable($factory)) {
+                    $this->bindings[$abstract] = $factory;
+
+                    return;
+                }
+
+                if (is_string($factory)) {
+                    $concrete = $factory;
+                    $this->bindings[$abstract] = static function () use ($concrete): object {
+                        return new $concrete();
+                    };
+                }
+            }
+
+            public function instance(string $abstract, object $instance): void
+            {
+                $this->instances[$abstract] = $instance;
+            }
+
+            /**
+             * @template T of object
+             * @param class-string<T> $abstract
+             * @return T
+             */
+            public function make(string $abstract): mixed
+            {
+                if (isset($this->instances[$abstract])) {
+                    /** @var T */
+                    return $this->instances[$abstract];
+                }
+
+                if (isset($this->bindings[$abstract])) {
+                    /** @var T */
+                    return $this->instances[$abstract] = ($this->bindings[$abstract])($this);
+                }
+
+                throw new \RuntimeException("No binding registered for {$abstract}.");
+            }
+        };
+
+        $provider = new HttpClientServiceProvider($container);
+        $provider->register();
+        $provider->boot();
+
+        return $container;
+    }
+
+    /**
+     * @return void
      */
     public function test_http_client_is_bound_in_container(): void
     {
-        $app = new Application();
-        $app->register(HttpClientServiceProvider::class);
-        $app->bootstrap();
+        $container = $this->makeBootedContainer();
 
-        $client = $app->make(HttpClient::class);
-
-        $this->assertInstanceOf(HttpClient::class, $client);
+        $this->assertInstanceOf(HttpClient::class, $container->make(HttpClient::class));
     }
 
     /**
-     * @throws ReflectionException
-     * @throws ApplicationException
-     * @throws ContainerException
+     * @return void
      */
     public function test_transport_interface_resolves_to_curl_transport(): void
     {
-        $app = new Application();
-        $app->register(HttpClientServiceProvider::class);
-        $app->bootstrap();
+        $container = $this->makeBootedContainer();
 
-        $transport = $app->make(TransportInterface::class);
-
-        $this->assertInstanceOf(CurlTransport::class, $transport);
+        $this->assertInstanceOf(CurlTransport::class, $container->make(TransportInterface::class));
     }
 
     /**
-     * @throws ReflectionException
-     * @throws ApplicationException
-     * @throws ContainerException
+     * @return void
      */
     public function test_static_facade_is_wired_after_bootstrap(): void
     {
-        $app = new Application();
-        $app->register(HttpClientServiceProvider::class);
-        $app->bootstrap();
+        $container = $this->makeBootedContainer();
 
-        $containerClient = $app->make(HttpClient::class);
-
-        $this->assertSame($containerClient, Http::getClient());
+        $this->assertSame($container->make(HttpClient::class), Http::getClient());
     }
 
     /**
+     * @return void
      */
     public function test_facade_get_returns_http_request(): void
     {
-        $app = new Application();
-        $app->register(HttpClientServiceProvider::class);
-        $app->bootstrap();
+        $this->makeBootedContainer();
 
-        $request = Http::get('https://example.com');
-
-        $this->assertInstanceOf(HttpRequest::class, $request);
+        $this->assertInstanceOf(HttpRequest::class, Http::get('https://example.com'));
     }
 }
